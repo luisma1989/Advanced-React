454d8b8d77b9765c50d9a915fe27fc1cf8a37135
Luis Manuel Fernandez Camacho <lmfernandez@fintonic.com>
2018-10-26 17:03:20 +0200

    first commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = () => (
+    <div>
+        <p>Home</p>
+        <Link href='/sell'>
+            sell        
+        </Link>
+    </div>
+)
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Sell = () => (
+    <div>
+        <p>Sell</p>
+        <Link href='/'>
+            Home        
+        </Link>
+    </div>
+)
+
+export default Sell;
