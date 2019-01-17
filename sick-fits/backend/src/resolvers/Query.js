06c1641ad7de69aa6a7ec970f86ada628b1a60c9
luis manuel fernandez <lmfintonic.com>
2019-01-17 11:32:04 +0100

    feat: add itemsConection query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,6 +1,8 @@
 const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   console.log('get Items');
   //   const items = await ctx.db.query.items();
