800023bfa010220518f21a76043fdaa4f4bfb830
luisma fernandez <luismanuel.fernandez.camacho@gmail.com>
2019-03-03 13:15:33 +0100

    feat: add query, mutation and schema of orders

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,32 +1,68 @@
 const { forwardTo } = require('prisma-binding');
 const { hasPermission } = require('../utils');
+
 const Query = {
-  // async items(parent, args, ctx, info) {
-  //   console.log('get Items');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
   me(parent, args, ctx, info) {
-    // check if there is a current ID user
+    // check if there is a current user ID
     if (!ctx.request.userId) {
       return null;
     }
-    return ctx.db.query.user({
-      where: { id: ctx.request.userId }
-    }, info);
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
   },
-  async users (parent, args, ctx, info) {
-    // Check if they are logedin
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
     if (!ctx.request.userId) {
-      throw new Error('Debes estar llogueado en la apllicación.')
+      throw new Error('You must be logged in!');
     }
+    console.log(ctx.request.userId);
     // 2. Check if the user has the permissions to query all the users
-    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
-    // 3. if the do, query alll the users!
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // 2. if they do, query all the users!
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || !hasPermission) {
+      throw new Error('You cant see this buddd');
+    }
+    // 4. Return the order
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('you must be signed in!');
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
   },
 };
 
